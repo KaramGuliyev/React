1939e5655946f051e48277b11e277fa1c8e8a743
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-18 22:59:39 +0400

    Styling-1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Counter } from "./Components/Counter/Counter";
 import { InteractiveWelcome } from "./Components/Welcome/InteractiveWelcome";
 import { ClickTracker } from "./Components/Counter/ClickTracker";
 import ToDoList from "./Components/List/ToDoList";
+import { Welcome } from "./Components/Welcome/Welcome";
 
 export class App extends React.Component {
   render() {
     return (
       <>
-        <InteractiveWelcome
+        <Welcome
+          className="welcome"
           name={this.props.name}
           age={this.props.age}
         />
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -2,18 +2,26 @@ import React from "react";
 import { Age } from "./Age";
 export class Welcome extends React.Component {
   render() {
-    const nameVal = this.props.name.props.children;
+    const nameVal =
+      this.props.name.props.children;
     return (
-      <>
-        {this.props.age === undefined || Number(this.props.age) === 0 || !this.props.age ? (
+      <div className={this.props.className}>
+        {this.props.age === undefined ||
+        Number(this.props.age) === 0 ||
+        !this.props.age ? (
           ""
-        ) : this.props.age > 18 && this.props.age < 65 && nameVal === "John" ? (
+        ) : this.props.age > 18 &&
+          this.props.age < 65 &&
+          nameVal === "John" ? (
           <h1>Welcome {this.props.name}!</h1>
         ) : (
-          <h1>Sorry {this.props.name}... you are not allowed here...</h1>
+          <h1>
+            Sorry {this.props.name}... you are
+            not allowed here...
+          </h1>
         )}
         <Age age={this.props.age} />
-      </>
+      </div>
     );
   }
 }
