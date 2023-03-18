500c1fdce6bec9a1c3bd4256d1a54e69fabf3cea
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-18 19:15:35 +0400

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Counter } from "./Counter";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { ClickTracker } from "./ClickTracker";
+import { Counter } from "./Components/Counter/Counter";
+import { InteractiveWelcome } from "./Components/Welcome/InteractiveWelcome";
+import { ClickTracker } from "./Components/Counter/ClickTracker";
 export class App extends React.Component {
   render() {
     return (
       <>
-        <InteractiveWelcome name={this.props.name} age={this.props.age} />
+        <InteractiveWelcome
+          name={this.props.name}
+          age={this.props.age}
+        />
         <Counter counterStart={25} />
         <ClickTracker />
       </>
