263ac2f221aa2caed2f67234a15391032d5be427
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-21 18:48:13 +0400

    Fix

--- src/Counter.js
+++ src/Counter.js
@@ -9,7 +9,6 @@ export class Counter extends React.Component {
     super(props);
     setInterval(() => {
       this.setState((state) => {
-        console.log(state);
         return { count: state.count + 1 };
       });
     }, 1000);
