c68784a1244ddf49cf138bb626d8f375d877388f
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-21 18:45:46 +0400

    Fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-root.render(<App name={<strong>John</strong>}  age={25} />);
+root.render(
+  <App name={<strong>John</strong>} age={25} />
+);
