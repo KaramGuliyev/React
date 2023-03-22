468e2c3aaf5cb1682f8474890f2fd4f465f376fa
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-23 00:08:17 +0400

    useCallback

--- src/CustomHooks/useCounter.js
+++ src/CustomHooks/useCounter.js
@@ -1,20 +1,23 @@
 // Create a custom hook useCounter that keeps track of the state of a counter, and returns the current value of the counter as well as three functions to increment,
 // decrement and reset the counter.
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function useCounter(initalValue = 0) {
   const [count, setCount] = useState(initalValue);
 
-  function handleInc() {
-    setCount(count + 1);
-  }
-  function handleDec() {
-    setCount(count - 1);
-  }
-  function handleReset() {
-    setCount(initalValue);
-  }
+  const handleInc = useCallback(function handleInc() {
+    setCount((count) => count + 1);
+  }, []);
+  const handleDec = useCallback(function handleDec() {
+    setCount((count) => count - 1);
+  }, []);
+  const handleReset = useCallback(
+    function handleReset() {
+      setCount(initalValue);
+    },
+    [initalValue]
+  );
   return {
     count: count,
     onIncrement: handleInc,
