e2f48cd052366a49b22379f402a8ff2c1432f79b
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-23 15:54:56 +0400

    ReactRoutes-2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import Container from "./Components/ComponentComposition/Container";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Components/Welcome/Welcome";
+import { Counter } from "./Components/Counter/Counter";
 
 export default function App(props) {
-  // Create an App component that wraps a Routes component and add a single Route to the / path that renders the Welcome component from
-  // Function Components 01, passing it a name prop. Render the App component within a BrowserRouter component.
-
+  // Add a new Route to the /counter path that renders the Counter component from useState 01.
   return (
     <Container title="My Awful App">
       <Routes>
         <Route path="/" element={<Welcome name={"Habibi"} age={35} />} />
+        <Route path="/counter" element={<Counter count={35} />} />
       </Routes>
     </Container>
   );
