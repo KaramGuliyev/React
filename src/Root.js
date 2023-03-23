fd5cc99f22e19f6be203932b723abdc33b6c0407
Karam Guliyev <karamguliyev10@gmail.com>
2023-03-23 16:25:01 +0400

    ReactRoutes-4

--- src/Root.js
+++ src/Root.js
@@ -1,22 +1,36 @@
 import React from "react";
 import Container from "./Components/ComponentComposition/Container";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Welcome from "./Components/Welcome/Welcome";
 import { Counter } from "./Components/Counter/Counter";
 import GithubUser from "./Components/GithubUser/GithubUser";
 
 export default function App(props) {
-  // Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and 
-  // renders the GithubUser component from useEffect 03 by passing it the received username.
-  
+  // Add three Links within the main App component and use them to navigate to all three routes.
+
   return (
     <Container title="My Awful App">
       <Routes>
-        <Route path="/" element={<Welcome name={"Habibi"} age={35} />} />
-        <Route path="/counter" element={<Counter count={35} />} />
-        <Route path="/users/:username" element={<GithubUser username={"KaramGuliyev"} />} />
-
+        <Route
+          path="/"
+          element={<Welcome name={"Habibi"} age={35} />}
+        />
+        <Route
+          path="/counter"
+          element={<Counter count={35} />}
+        />
+        <Route
+          path="/users/:username"
+          element={<GithubUser username={"KaramGuliyev"} />}
+        />
       </Routes>
+      <br />
+      <Link to={"/"}>Home</Link>
+      <br />
+      <Link to={"/counter"}>Counter</Link>
+      <br />
+      <Link to={"/users/KaramGuliyev"}>Habibi's Github</Link>
+      <br />
     </Container>
   );
 }
